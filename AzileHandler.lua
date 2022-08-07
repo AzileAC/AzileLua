f032f4a236b141dff6d9571fb83b51a1508d5de5
JonaxDevelopment <45129587+JonaxDevelopment@users.noreply.github.com>
2022-08-07 18:15:11 +0100

    Create AzileHandler.lua

---
+++ AzileHandler.lua
@@ -0,0 +1,23 @@
+--[[
+                                                                                   
+                                                                                   
+   ,---,                               ,--,                ,---,         ,----..   
+  '  .' \                     ,--,   ,--.'|               '  .' \       /   /   \  
+ /  ;    '.           ,----,,--.'|   |  | :              /  ;    '.    |   :     : 
+:  :       \        .'   .`||  |,    :  : '             :  :       \   .   |  ;. / 
+:  |   /\   \    .'   .'  .'`--'_    |  ' |      ,---.  :  |   /\   \  .   ; /--`  
+|  :  ' ;.   : ,---, '   ./ ,' ,'|   '  | |     /     \ |  :  ' ;.   : ;   | ;     
+|  |  ;/  \   \;   | .'  /  '  | |   |  | :    /    /  ||  |  ;/  \   \|   : |     
+'  :  | \  \ ,'`---' /  ;--,|  | :   '  : |__ .    ' / |'  :  | \  \ ,'.   | '___  
+|  |  '  '--'    /  /  / .`|'  : |__ |  | '.'|'   ;   /||  |  '  '--'  '   ; : .'| 
+|  :  :        ./__;     .' |  | '.'|;  :    ;'   |  / ||  :  :        '   | '/  : 
+|  | ,'        ;   |  .'    ;  :    ;|  ,   / |   :    ||  | ,'        |   :    /  
+`--''          `---'        |  ,   /  ---`-'   \   \  / `--''           \   \ .'   
+                             ---`-'             `----'                   `---`     
+                                                                                   
+COPYRIGHT JONAX BLAH BLAH FINISH LATER
+Designed & Coded by: Jonax
+]]
+local AzileACModule = require(script.AzileACModule)
+local AzileACSettings = require(script.AzileACSettings) 
+AzileACModule.Initialise("JOANXISHOT",AzileACSettings,1)
